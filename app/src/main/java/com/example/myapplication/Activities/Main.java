046cd201aaeb8440b8596c19d286d6fcf134cd08
RaneAce <eytanbarlev@gmail.com>
2024-05-31 20:48:31 +0300

    Added a popup helper screen in the main screen. Instructions and stuff

--- app/src/main/java/com/example/myapplication/Activities/Main.java
+++ app/src/main/java/com/example/myapplication/Activities/Main.java
@@ -1,21 +1,33 @@
 package com.example.myapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.PopupWindow;
+
 import com.example.myapplication.R;
 
 public class Main extends AppCompatActivity {
 
     ImageButton Settings, Shop, ScoreBoard, Screen;
 
+    ConstraintLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.ConstraintLayout);
 
         Screen = findViewById(R.id.Screen);
         Settings = findViewById(R.id.Settings);
@@ -49,7 +61,6 @@ public class Main extends AppCompatActivity {
                 openScoreBoard();
             }
         });
-
     }
 
     public void openScreen() {
@@ -70,5 +81,44 @@ public class Main extends AppCompatActivity {
     public void openScoreBoard() {
         Intent intent = new Intent(this, com.example.myapplication.Activities.ScoreBoard.class);
         startActivity(intent);
+    }
+
+    public void help_click(View view) {
+        CreatepopUpwindow();
+    }
+
+    public void CreatepopUpwindow (){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        View popUpView = inflater.inflate(R.layout.help_popup,null);
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.MATCH_PARENT;
+        boolean focusable = true;
+
+        PopupWindow popupWindow = new PopupWindow(popUpView,width,height,focusable);
+
+        layout.post(new Runnable() {
+            @Override
+            public void run() {
+                popupWindow.showAtLocation(layout, Gravity.CENTER,0,0);
+            }
+        });
+
+        Button okay = popUpView.findViewById(R.id.okay);
+
+        okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+
+        popUpView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
