535119e7da83a139f7057b7a9558222b72648a67
RaneAce <eytanbarlev@gmail.com>
2024-06-19 12:27:27 +0300

    changed in settings instead of showing uid, shows username of current connected user

--- app/src/main/java/com/example/myapplication/Activities/Settings.java
+++ app/src/main/java/com/example/myapplication/Activities/Settings.java
@@ -1,21 +1,30 @@
 package com.example.myapplication.Activities;
 
 import static com.example.myapplication.Database.FBref.refAuth;
+import static com.example.myapplication.Database.FBref.refUser;
+import com.example.myapplication.Database.User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.myapplication.R;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class Settings extends AppCompatActivity {
 
-    private TextView uid_text;
+    private TextView username_text;
     private String uid;
     private FirebaseUser User = refAuth.getCurrentUser();
     public SharedPreferences user_preferences;
@@ -25,11 +34,36 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        uid_text = (TextView) findViewById(R.id.uid_text);
+        username_text = (TextView) findViewById(R.id.username_text);
         user_preferences = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
 
         uid = User.getUid();
-        uid_text.setText(uid);
+
+        refUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            String username;
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds : snapshot.getChildren()){
+                    User user_information = ds.getValue(User.class);
+                    if(user_information != null && Objects.equals(user_information.getUid(), uid)){
+                        username = user_information.getUsername();
+                    }
+                    else{
+                        Log.d("Firebase", "snapshot is null for Key: " + ds.getKey());
+                        Log.d("Firebase", "Or UID wasn't right");
+                    }
+                }
+                username_text.setText(username);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
     }
 
     public void logout(View view){
